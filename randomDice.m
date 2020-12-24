1587136a86b705b7a9aac09dac32d47ccdc9e310
bichenggu <71300666+bichenggu@users.noreply.github.com>
2020-12-25 05:41:30 +0800

    Add files via upload

--- randomDice.m
+++ randomDice.m
@@ -1,72 +1,77 @@
 function randomDice()
-    format long
-    P1(36)=0;
-    P2(36)=0;
-    mysum=0;
+    % input: None
+    % output: some floats (the probability of winning, losing ,or a tied game)
+    %
+    % This function can compute the probability of one of the players of
+    % winning or losing the dice game. There are two players. 
+    %
+    %%  You may set two arguments on your own.
+    %  one is how many dices player has (player_X_NumOfDice),
+    %  and the other is the max point of one dice (player_X_dicePoint).
+    %  
+    %   At first, player_1_NumOfDice is set to be 9,
+    %             player_1_dicePoint is set to be 4,
+    %             player_2_NumOfDice is set to be 6,
+    %             player_2_dicePoint is set to be 6.    
     
-    %{
-    for qq=1:4
-        for ww=1:4 
-            for ee=1:4
-                for rr=1:4
-                    for tt=1:4
-                        for yy=1:4
-                            for uu=1:4
-                                for ii=1:4
-                                    for pp=1:4
-                                        k=qq+ww+ee+rr+tt+yy+uu+ii+pp;
-                                        P1(k)=P1(k)+1;
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-    %}
-    P1=num_dice(9,4);
-    %{
-    for tt=1:6
-        for yy=1:6
-            for uu=1:6
-                for ii=1:6
-                    for jj=1:6 
-                        for pp=1:6
-                            k=tt+yy+uu+ii+jj+pp;
-                            P2(k)=P2(k)+1;
-                        end
-                    end
-                end
-            end
-        end
-    end   
-    %}
-    P2=num_dice(6,6);
-    for ii=9:36
-        for jj=6:36
+    %% Set initial value (for user)
+    player_1_NumOfDice=9;           
+    player_1_dicePoint=4;
+    
+    player_2_NumOfDice=6;
+    player_2_dicePoint=6;
+    
+    %% Computing...
+    format long
+    mysumW=0;
+    mysumT=0;
+    mysumL=0;
+    P1=num_dice(player_1_NumOfDice,player_1_dicePoint);
+    P2=num_dice(player_2_NumOfDice,player_2_dicePoint);
+    
+    for ii=player_1_NumOfDice:(player_1_NumOfDice*player_1_dicePoint)
+        for jj=player_2_NumOfDice:(player_2_NumOfDice*player_2_dicePoint)
             if ii>jj
-                mysum=mysum+P1(ii)*P2(jj);
+                mysumW=mysumW+P1(ii)*P2(jj);
+            elseif ii==jj
+                mysumT=mysumT+P1(ii)*P2(jj);
             else
-                break;
+                mysumL=mysumL+P1(ii)*P2(jj);
             end
         end
     end
-    %disp(mysum)
-    p=mysum/(6^6)/(4^9);
-    ppre=roundn(p,-7);
+    probabilityW=mysumW/(player_2_dicePoint^player_2_NumOfDice)/(player_1_dicePoint^player_1_NumOfDice);
+    probabilityT=mysumT/(player_2_dicePoint^player_2_NumOfDice)/(player_1_dicePoint^player_1_NumOfDice);
+    probabilityL=mysumL/(player_2_dicePoint^player_2_NumOfDice)/(player_1_dicePoint^player_1_NumOfDice);
+    
+    %% Set precision and output the result
+    ppreW=roundn(probabilityW,-7);
+    ppreT=roundn(probabilityT,-7);
+    ppreL=roundn(probabilityL,-7);
     digits(9)
-    vpa(ppre)
-
+    disp('The winning probability of Peter is approximately') 
+    disp(vpa(ppreW))
+    
+    disp('The probability of a tied game is approximately') 
+    disp(vpa(ppreT))
+    disp('The losing probability of Peter is approximately') 
+    disp(vpa(ppreL))
 end
 
-function pro=num_dice(n,d)
-    p(n*d)=0;
+function probabibity=num_dice(n,d)   
+    %% input: n (how many dices), d (max dice point)
+    % output: probabibity(a vector) 
+    % Compute how many times do the number between n and n*d appear     
+    %
+    % A recursive function.
     
     for ii=1:n
+        for mm=1:(ii*d)
+            p(mm)=0;
+        end
+        
         if ii==1
-            pro=ones(1,d);
+            probabibity=ones(1,d);
         else            
             frontPro=num_dice(ii-1,d);
             frontStart=ii-1;
@@ -75,11 +80,8 @@ function pro=num_dice(n,d)
                 for kk=frontStart:frontFinal
                     p(jj+kk)=p(jj+kk)+frontPro(kk);
                 end
-            end
-            
-            pro=p;
-        end     
-        
+            end            
+            probabibity=p;                          
+        end           
     end
-
 end
